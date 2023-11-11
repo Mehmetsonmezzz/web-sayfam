a511967ef7f664f68b8a46e91f11b3303f116892
Mehmet <mehmetsonmez216@gmail.com>
2023-11-11 06:21:39 +0300

    App.js fixed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
-import React, { useState, useEffect } from "react";
 import "./App.css";
-import { useLanguage } from "./contexts/LanguageContext";
-import axios from "axios";
-import en from "./languages/en";
 import Header from "./pages/Header";
 import Footer from "./pages/Footer";
-
-import { useTheme } from "./contexts/ThemeContext";
 import Skills from "./pages/Skills";
 import Profile from "./pages/Profile";
 import Projects from "./pages/Projects";
@@ -14,21 +8,11 @@ import Projects from "./pages/Projects";
 function App() {
   return (
     <div>
-      {/* 
-      <Skills />
-       */}
-      {/* <Header /> */}
       <Header />
       <Skills />
       <Profile />
       <Projects />
       <Footer />
-      {/* <div>
-        <button onClick={languageHandler}>{t("language")}</button>
-        <h1>{t("welcome")}</h1>
-        <h1>{translatedText}</h1>
-        <p>{t("about")}</p>
-      </div> */}
     </div>
   );
 }
